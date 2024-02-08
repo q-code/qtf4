3ae9eec71d1cf85fd0fbfa5552f9105b0d96f067
q-code <phvandenberghe@gmail.com>
2024-02-08 10:36:59 +0100

    V4

--- install/tool_check.php
+++ install/tool_check.php
@@ -9,73 +9,55 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.or
 <title>'.THISAPPNAME.' installation checker</title>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
 <link rel="shortcut icon" href="qt.ico" />
-<style type="text/css">
-div.page {margin:5px auto;width:700px}
-#banner {color:inherit;background:linear-gradient(to bottom,#0C4C8C 20%,#156AC2)}
-#logo {padding:5px}
-div.body {padding:10px; border:solid 1px #156AC2}
-h1,h2,p,a,select,input,textarea,td,th,fieldset,div {font-family:Verdana, Arial, sans-serif}
-h1 {margin-top:10px; margin-bottom:5px; font-size:14pt; font-weight:bold}
-h2 {margin-top:10px; margin-bottom:5px; font-size:12pt; font-weight:bold}
-p,select,input,textarea,td,th,a,fieldset {font-size:9pt; text-decoration:none}
+<style type="text/css">*{box-sizing:border-box;margin:0}
+header,main{margin:1rem auto;width:650px;font-family:system-ui,sans-serif}
+header{padding:0.5rem;color:inherit;background:linear-gradient(to bottom,#0C4C8C 20%,#156AC2)}
+footer{padding-top:0.75rem;text-align:center}
+h1{margin:0.5rem 0 0.4rem 0;font-size:1.4rem}
 a {color:#0000FF}
-a.visited {color:#0000FF}
-a:hover {color:#0000FF; text-decoration:underline}
 p.tool_check {margin:5px 0 0 0; padding:0}
 p.endcheck {margin:5px 0 0 0; padding:5px; border:solid 1px #aaaaaa}
-span.ok {color:#00aa00; background-color:inherit}
-span.nok {color:#ff0000; background-color:inherit}
+.ok {color:#00aa00; background-color:inherit}
+.nok {color:#ff0000; background-color:inherit}
 </style>
 </head>
-
 <body>
-
-
-<div class="page">
-
-
-<div id="banner">
+<header>
 <img id="logo" src="'.$root.'bin/css/qtf_logo.gif" width="175" height="50" style="border-width:0" alt="'.THISAPPNAME.'" title="'.THISAPPNAME.'"/>
-</div>
-
-
-<div class="body">
+</header>
+<main>
 ';
 
 // ------
 // 1 CONFIG
 // ------
-echo '<p style="text-align:right">'.THISAPPNAME.' 4.0 build:20230618</p>';
-
-echo '<p style="text-align:right"><a href="setup.php">Install...</a>';
-if ( file_exists('tool_tables.php') ) echo ' | <a href="tool_tables.php">Tool tables...</a>';
-echo '</p>';
 
 echo '<h1>Checking your configuration</h1>';
+echo '<p>'.THISAPPNAME.' 4.0 build:20230618</p>';
 
 $error = '';
+$result = true;
 
-// 1 file exist
+//======
+try {
+//======
 
+  // 1 file exist
   echo '<p class="tool_check">Checking installed files... ';
-
-  if ( !file_exists($root.'config/config_db.php') ) $error .= 'File <b>config_db.php</b> is not in the <b>config</b> directory. Communication with database is impossible.<br>';
-  if ( !file_exists($root.'bin/init.php') ) $error .= 'File <b>init.php</b> is not in the <b>bin</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/lib_qt_core.php') ) $error .= 'File <b>lib_qt_core.php</b> is not in the <b>bin</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/lib_qtf_base.php') ) $error .= 'File <b>lib_qtf_base.php</b> is not in the <b>bin</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/class/class.qt.db.php') ) $error .= 'File <b>class.qt.db.php</b> is not in the <b>bin/class</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/class/class.qt.core.php') ) $error .= 'File <b>class.qt.core.php</b> is not in the <b>bin/class</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/class_qtf_section.php') ) $error .= 'File <b>class_qtf_section.php</b> is not in the <b>bin/class</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/class_qtf_topic.php') ) $error .= 'File <b>class_qtf_topic.php</b> is not in the <b>bin/class</b> directory. Application cannot start.<br>';
-  if ( !file_exists($root.'bin/class_qtf_post.php') ) $error .= 'File <b>class_qtf_post.php</b> is not in the <b>bin/class</b> directory. Application cannot start.<br>';
-
-  if ( empty($error) )
-  {
-  echo '<span class="ok">Main files found.</span></p>';
-  }
-  else
-  {
-  die('<span class="nok">'.$error.'</span></p>');
+  if ( !file_exists($root.'config/config_db.php') ) $error .= 'File config_db.php is not in the config directory. Communication with database is impossible. ';
+  if ( !file_exists($root.'bin/init.php') ) $error .= 'File init.php is not in the bin directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/lib_qt_core.php') ) $error .= 'File lib_qt_core.php is not in the bin directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/lib_qtf_base.php') ) $error .= 'File lib_qtf_base.php is not in the bin directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/class/class.qt.db.php') ) $error .= 'File class.qt.db.php is not in the bin/class directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/class/class.qt.core.php') ) $error .= 'File class.qt.core.php is not in the bin/class directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/class_qtf_section.php') ) $error .= 'File class_qtf_section.php is not in the bin/class directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/class_qtf_topic.php') ) $error .= 'File class_qtf_topic.php is not in the bin/class directory. Application cannot start. ';
+  if ( !file_exists($root.'bin/class_qtf_post.php') ) $error .= 'File class_qtf_post.php is not in the bin/class directory. Application cannot start. ';
+  if ( empty($error) ) {
+    echo '<span class="ok">Main files found.</span></p>';
+  } else {
+    echo '</p>';
+    throw new Exception( $error );
   }
 
 // 2 config is correct
@@ -86,28 +68,16 @@ $error = '';
   include $root.'config/config_cst.php';
   include $root.'bin/lib_qtf_base.php';
 
-  if ( !defined('QDB_SYSTEM') )   $error .= 'Variable <b>QDB_SYSTEM</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
-  if ( !defined('QDB_HOST') )     $error .= 'Variable <b>QDB_HOST</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
-  if ( !defined('QDB_DATABASE') ) $error .= 'Variable <b>QDB_DATABASE</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
-  if ( !defined('QDB_PREFIX') )   $error .= 'Variable <b>QDB_PREFIX</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
-  if ( !defined('QDB_USER') )     $error .= 'Variable <b>QDB_USER</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
-  if ( !defined('QDB_PWD') )      $error .= 'Variable <b>QDB_PWD</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.<br>';
+  if ( !defined('QDB_SYSTEM') )   throw new Exception( 'Variable QDB_SYSTEM is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !defined('QDB_HOST') )     throw new Exception( 'Variable QDB_HOST is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !defined('QDB_DATABASE') ) throw new Exception( 'Variable QDB_DATABASE is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !defined('QDB_PREFIX') )   throw new Exception( 'Variable QDB_PREFIX is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !defined('QDB_USER') )     throw new Exception( 'Variable QDB_USER is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !defined('QDB_PWD') )      throw new Exception( 'Variable QDB_PWD is not defined in the file config/config_db.php. Communication with database is impossible.');
+  if ( !in_array(QDB_SYSTEM, ['pdo.mysql','mysql','pdo.sqlsrv','sqlsrv','pdo.pg','pg','pdo.sqlite','sqlite','pdo.oci','oci']) ) throw new Exception('Unknown db type '.QDB_SYSTEM);
+  if ( empty(QDB_DATABASE) )  throw new Exception( 'Variable QDB_DATABASE is not defined in the file config/config_db.php. Communication with database is impossible.');
 
-  if ( !empty($error) )  die('<span class="nok">'.$error.'</span>');
-
-  // tool_check db type
-  if ( !in_array(QDB_SYSTEM,array('pdo.mysql','mysql','pdo.sqlsrv','sqlsrv','pdo.pg','pg','pdo.sqlite','sqlite','pdo.oci','oci')) ) die('Unknown db type '.QDB_SYSTEM);
-  // tool_check other values
-  if ( empty(QDB_DATABASE) )  $error .= '<br>Variable <b>QDB_DATABASE</b> is not defined in the file <b>config/config_db.php</b>. Communication with database is impossible.';
-
-  if ( empty($error) )
-  {
   echo '<span class="ok">Done.</span></p>';
-  }
-  else
-  {
-  die('<span class="nok">'.$error.'</span></p>');
-  }
 
 // 3 test db connection
 
@@ -115,16 +85,9 @@ $error = '';
 
   include $root.'bin/class/class.qt.db.php';
 
-  $oDB = new CDatabase(QDB_SYSTEM,QDB_HOST,$database,QDB_USER,QDB_PWD);
+  $oDB = new CDatabase(QDB_SYSTEM, QDB_HOST, $database, QDB_USER, QDB_PWD);
 
-  if ( empty($oDB->error) )
-  {
   echo '<span class="ok">Done.</span></p>';
-  }
-  else
-  {
-  die('<span class="nok">Connection with database failed.<br>Check that server is up and running.<br>Check that the settings in the file <b>config/config_db.php</b> are correct for your database.</span></p>');
-  }
 
 // end CONFIG tests
 
@@ -136,7 +99,7 @@ $error = '';
 $error = '';
 
 echo '
-<h1>Checking your database design</h1>
+<h1>Checking your database</h1>
 ';
 
 // 1 setting table
@@ -144,43 +107,42 @@ echo '
   echo '<p class="tool_check">Checking setting table... ';
 
   $oDB->query( 'SELECT setting FROM '.QDB_PREFIX.'qtasetting WHERE param="version"');
-  if ( !empty($oDB->error) ) die('<span class="nok">Problem with table '.QDB_PREFIX.'qtasetting</span>');
   $row = $oDB->getRow();
-  $strVersion = $row['setting'];
+  $version = $row['setting'];
 
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtasetting] exists. Version is ',$strVersion,'.</span>';
-  if ( !in_array(substr($strVersion,0,3),array('3.0','3.1','4.0')) ) die('<span class="nok">But data in this table refers to an incompatible version (must be version 3.0).</span></p>');
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtasetting] exists. Version: '.$version.'.</span>';
+  if ( substr($version,0,3)!=='4.0' ) throw new Exception('Database version is incompatible (must be version 4.x)');
   echo '</p>';
 
 // 2 domain table
 
   echo '<p class="tool_check">Checking domain table... ';
   $intCount = $oDB->count( QDB_PREFIX.'qtadomain' );
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtadomain] exists. ',$intCount,' domain(s) found.</span></p>';
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtadomain] exists. '.$intCount.' domain(s) found.</span></p>';
 
 // 3 team table
 
   echo '<p class="tool_check">Checking forum table...';
   $intCount = $oDB->count( QDB_PREFIX.'qtaforum' );
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtaforum] exists. ',$intCount,' section(s) found.</span></p>';
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtaforum] exists. '.$intCount.' section(s) found.</span></p>';
 
 // 4 topic table
 
   echo '<p class="tool_check">Checking topic table...';
   $intCount = $oDB->count( QDB_PREFIX.'qtatopic' );
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtatopic] exists. ',$intCount,' topic(s) found.</span></p>';
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtatopic] exists. '.$intCount.' topic(s) found.</span></p>';
 
 // 5 post table
 
   echo '<p class="tool_check">Checking post table...';
   $intCount = $oDB->count( QDB_PREFIX.'qtapost' );
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtapost] exists. ',$intCount,' post(s) found.</span></p>';
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtapost] exists. '.$intCount.' post(s) found.</span></p>';
 
 // 6 user table
 
   echo '<p class="tool_check">Checking user table... ';
   $intCount = $oDB->count( QDB_PREFIX.'qtauser' );
-  echo '<span class="ok">Table [',QDB_PREFIX,'qtauser] exists. ',$intCount,' user(s) found.</span></p>';
+  echo '<span class="ok">Table ['.QDB_PREFIX.'qtauser] exists. '.$intCount.' user(s) found.</span></p>';
 
 // end DATABASE tests
 
@@ -200,19 +162,19 @@ echo '
   $oDB->query( 'SELECT setting FROM '.QDB_PREFIX.'qtasetting WHERE param="language"');
   $row = $oDB->getRow();
   $str = $row['setting'];
-  if ( empty($str) ) $error .= 'Setting <b>language</b> is not defined in the setting table. Application can only work with english.<br>';
-  if ( !file_exists($root."language/$str/lg_main.php") ) $error .= "File <b>lg_main.php</b> is not in the <b>language/xx</b> directory.<br>";
-  if ( !file_exists($root."language/$str/lg_adm.php") )  $error .= "File <b>lg_adm.php</b> is not in the <b>language/xx</b> directory.<br>";
-  if ( !file_exists($root."language/$str/lg_icon.php") ) $error .= "File <b>lg_icon.php</b> is not in the <b>language/xx</b> directory.<br>";
-  if ( !file_exists($root."language/$str/lg_reg.php") )  $error .= "File <b>lg_reg.php</b> is not in the <b>language/xx</b> directory.<br>";
-  if ( !file_exists($root."language/$str/lg_zone.php") ) $error .= "File <b>lg_zone.php</b> is not in the <b>language/xx</b> directory.<br>";
+  if ( empty($str) ) $error .= 'Setting language is not defined in the setting table. Application can only work with english. ';
+  if ( !file_exists($root."language/$str/lg_main.php") ) $error .= "File lg_main.php is not in the language/xx directory. ";
+  if ( !file_exists($root."language/$str/lg_adm.php") )  $error .= "File lg_adm.php is not in the language/xx directory. ";
+  if ( !file_exists($root."language/$str/lg_icon.php") ) $error .= "File lg_icon.php is not in the language/xx directory. ";
+  if ( !file_exists($root."language/$str/lg_reg.php") )  $error .= "File lg_reg.php is not in the language/xx directory. ";
+  if ( !file_exists($root."language/$str/lg_zone.php") ) $error .= "File lg_zone.php is not in the language/xx directory. ";
   if ( $str!='english' )
   {
-  if ( !file_exists($root."language/en/lg_main.php") ) $error .= "File <b>lg_main.php</b> is not in the <b>language/en</b> directory. English language is mandatory.<br>";
-  if ( !file_exists($root."language/en/lg_adm.php") )  $error .= "File <b>lg_adm.php</b> is not in the <b>language/en</b> directory. English language is mandatory.<br>";
-  if ( !file_exists($root."language/en/lg_icon.php") ) $error .= "File <b>lg_icon.php</b> is not in the <b>language/en</b> directory. English language is mandatory.<br>";
-  if ( !file_exists($root."language/en/lg_reg.php") )  $error .= "File <b>lg_reg.php</b> is not in the <b>language/en</b> directory. English language is mandatory.<br>";
-  if ( !file_exists($root."language/en/lg_zone.php") ) $error .= "File <b>lg_zone.php</b> is not in the <b>language/en</b> directory. English language is mandatory.<br>";
+  if ( !file_exists($root."language/en/lg_main.php") ) $error .= "File lg_main.php is not in the language/en directory. English language is mandatory. ";
+  if ( !file_exists($root."language/en/lg_adm.php") )  $error .= "File lg_adm.php is not in the language/en directory. English language is mandatory. ";
+  if ( !file_exists($root."language/en/lg_icon.php") ) $error .= "File lg_icon.php is not in the language/en directory. English language is mandatory. ";
+  if ( !file_exists($root."language/en/lg_reg.php") )  $error .= "File lg_reg.php is not in the language/en directory. English language is mandatory. ";
+  if ( !file_exists($root."language/en/lg_zone.php") ) $error .= "File lg_zone.php is not in the language/en directory. English language is mandatory. ";
   }
 
   $oDB->query( 'SELECT setting FROM '.QDB_PREFIX.'qtasetting WHERE param="skin_dir"');
@@ -223,13 +185,10 @@ echo '
   if ( !file_exists($root."$str/qtf_styles.css") ) $error .= "File <b>qtf_styles.css</b> is not in the <b>$str</b> directory.<br>";
   if ( !file_exists($root."skin/default/qtf_styles.css") ) $error .= 'File <b>qtf_styles.css</b> is not in the <b>skin/default</b> directory. Default skin is mandatory.<br>';
 
-  if ( empty($error) )
-  {
-  echo '<span class="ok">Ok.</span>';
-  }
-  else
-  {
-  echo '<span class="nok">',$error,'</span>';
+  if ( empty($error) ) {
+    echo '<span class="ok">Done.</span>';
+  } else {
+    echo '<span class="nok">'.$error.'</span>';
   }
 
   echo '</p>';
@@ -243,9 +202,7 @@ echo '
 // ------
 $error = '';
 
-echo '
-<h1>Administration tips</h1>
-';
+echo '<h1>Administration tips</h1>';
 
 // 1 admin email
 
@@ -327,26 +284,34 @@ echo '<p class="endcheck">Administration tips completed.</p>';
 // ------
 // 5 END
 // ------
-echo '
-<h1>Result</h1>
-<p class="tool_check">The checker did not found blocking issues in your configuration.<br>';
-
   $oDB->query( 'SELECT setting FROM '.QDB_PREFIX.'qtasetting WHERE param="board_offline"');
   $row = $oDB->getRow();
-  $strOff = $row['setting'];
-  if ( $strOff=='1' ) echo 'Your board seams well installed, but is currently <font color="red">off-line</font>.<br>Log as Administrator and go to the Administration panel to turn your board on-line.<br>';
+  if ( $row['setting']==='1' ) echo '<p>Your board seams well installed, but is currently <font color="red">off-line</font>.<br>Log as Administrator and go to the Administration panel to turn your board on-line.</p>';
 
-echo '</p><p>';
-if ( is_dir($root.'install') ) echo '<a href="setup_9.php">Secure your installation...</a> | ';
-echo '<a href="'.$root.'qtf_index.php">Go to '.THISAPPNAME.'</a></p>';
+//======
+} catch (Exception $e) {
+//======
+
+echo '<p class="nok">'.$e->getMessage().'</p>';
+$result = false;
+
+//======
+}
+//======
+
+echo '<h1>Result</h1>';
+echo '<p>The checker did '.($result ? 'not ' : '').'found blocking issues in your configuration.</p>';
 
 // ------
 // HTML END
 // ------
+$menu = [];
+if ( $result && is_dir($root.'install') ) $menu[] = '<a href="setup_9.php">Secure your installation</a>';
+if ( $result && file_exists('tool_tables.php') ) $menu[] = '<a href="tool_tables.php">Tool tables</a>';
+$menu[] = '<a href="setup.php">Install</a>';
+$menu[] = '<a href="'.$root.'qtf_index.php">Go to '.THISAPPNAME.'</a>';
+echo '<footer>'.implode(' | ', $menu).'</footer>';
 echo '
-</div>
-
-
-</div>
+</main>
 </body>
 </html>';
