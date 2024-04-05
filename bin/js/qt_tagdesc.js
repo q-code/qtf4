fa266ad6b2245acb712f619cdbdd8d5d51e83beb
q-code <phvandenberghe@gmail.com>
2024-04-05 22:20:10 +0200

    V4

--- bin/js/qt_tagdesc.js
+++ bin/js/qt_tagdesc.js
@@ -1,27 +1,31 @@
 /*
-Event mouseover calls bin/srv_tagdesc.php:
-Attribute data-tagdesc must contain the searched tag
-When found, the description is transfered to the title while the data-tagdesc is cleared.
-Other elements in the document having the same tagdesc are also changed
+This adds an mouseover-event (calling srv_tagdesc.php) to every element with the attribute [data-tagdesc].
+[data-tagdesc] must contain the searched tag. When found, the description is transfered in the [title] while [data-tagdesc] is cleared.
+Other elements in the document having the same [data-tagdesc] are also changed.
+TIPS: This <script src="qt_tagdesc.js"> supports options passed as attribute data-{dir|lang|s|xs|sep|ci|na}
 */
 const tags = document.querySelectorAll('[data-tagdesc]');
 if ( tags.length>0 ) {
-  const tag_dir = document.currentScript.dataset['dir'];
-  const tag_lang = document.currentScript.dataset['lang'];
-  const tag_s = document.currentScript.dataset['s'];
-  const tag_cs = document.currentScript.dataset['cs']; // cross-sections search
-  const tag_sep = document.currentScript.dataset['sep']; // tag-description separator
-  const uri = ( tag_s ? '&s='+tag_s : '' ) + ( tag_cs ? '&cs=1' : '' ) + ( tag_dir ? '&dir='+tag_dir : '' ) + ( tag_lang ? '&lang='+tag_lang : '' ) + ( tag_sep ? '&sep='+encodeURI(tag_sep) : '' );
-  tags.forEach( tag => {
-    tag.addEventListener('mouseover', () => {
-      const fv = tag.dataset.tagdesc; if ( fv==='' ) return; // already described
-      fetch( 'bin/srv_tagdesc.php?fv='+encodeURI(fv)+uri )
-      .then( response => response.text() )
-      .then( desc => {
-        const siblings = document.querySelectorAll('[data-tagdesc="'+fv+'"]');
-        siblings.forEach( sibling => { sibling.title = fv+desc; sibling.dataset.tagdesc = ''; } );
-        } )
-      .catch( err => console.log(err) );
-    }, {once:true} );
-  });
+
+const cs_dir = document.currentScript.dataset['dir']; // default repository of csv-files
+const cs_lang = document.currentScript.dataset['lang']; // current langue (iso)
+const cs_s = document.currentScript.dataset['s']; // section id
+const cs_xs = document.currentScript.dataset['xs']; // cross-sections search (default:true)
+const cs_sep = document.currentScript.dataset['sep']; // tag-description separator
+const cs_ci = document.currentScript.dataset['ci']; // case insensitive (default:true)
+const cs_na = document.currentScript.dataset['na']; // show 'no description' (default:false)
+const uri = ( cs_s ? '&s='+cs_s : '' ) + ( cs_xs==='0' ? '&xs=0' : '' ) + ( cs_dir ? '&dir='+cs_dir : '' ) + ( cs_lang ? '&lang='+cs_lang : '' ) + ( cs_sep ? '&sep='+encodeURI(cs_sep) : '' ) + ( cs_ci==='0' ? '&ci=0' : '' ) + ( cs_na==='1' ? '&na=1' : '' );
+tags.forEach( tag => {
+  tag.addEventListener('mouseover', () => {
+    const fv = tag.dataset.tagdesc; if ( fv==='' ) return; // already described
+    fetch( 'bin/srv_tagdesc.php?fv='+encodeURI(fv)+uri )
+    .then( response => response.text() )
+    .then( desc => {
+      const siblings = document.querySelectorAll('[data-tagdesc="'+fv+'"]');
+      siblings.forEach( sibling => { sibling.title = fv+desc; sibling.dataset.tagdesc = ''; } );
+      } )
+    .catch( err => console.log(err) );
+  }, {once:true} );
+});
+
 }
--- bin/srv_tagdesc.php
+++ bin/srv_tagdesc.php
@@ -1,17 +1,19 @@
 <?php // v4.0 build:20240210
-
-// SERVEUR SCRIPT
-// Perform async queries on request from web pages (ex: using ajax) with GET method
-// Ouput (echo) results as string (or 'no description')
-
+/*
+SERVEUR SCRIPT [async]. GET arguments {fv|s|dir|lang|ci|xs|na|sep}
+Perform queries to search a tag description
+Ouput (echo) results as string (or 'no description')
+*/
 if ( empty($_GET['fv']) ) { echo '[missing data]'; exit; }
 $fv = $_GET['fv']; // tag to search
-$s = isset($_GET['s']) ? (int)$_GET['s'] : -1;
-$dir = isset($_GET['dir']) ? $_GET['dir'] : 'upload/';
-$lang = isset($_GET['lang']) ? $_GET['lang'] : 'en';
-$ci = isset($_GET['ci']) && $_GET['ci']!=='1' ? false : true; // default is case-insensitive
-$na = isset($_GET['na']); // On no description, TRUE show 'no description', FALSE invisible
+$s = isset($_GET['s']) ? (int)$_GET['s'] : -1; // section id (or -1 only common tags)
+$dir = isset($_GET['dir']) ? $_GET['dir'] : 'upload/'; // csv tagfiles repository
+$lang = isset($_GET['lang']) ? $_GET['lang'] : 'en'; // langue
+$ci = isset($_GET['ci']) && $_GET['ci']==='0' ? false : true; // default case-insensitive
+$xs = isset($_GET['xs']) && $_GET['xs']==='0' ? false : true; // default cross-section
+$na = isset($_GET['na']) && $_GET['na']==='1' ? true : false; // default void on description not found
 $sep = isset($_GET['sep']) ? $_GET['sep'] : ' - ';
+
 include 'lib_qt_tags.php';
 
 // Search in specific section
@@ -19,20 +21,17 @@ if ( $s>=0 ) {
   $desc = findInTagsFile('../'.$dir.'tags_'.$lang.'_'.$s.'.csv', $fv, $ci);
   if ( !empty($desc) ) { echo $sep.$desc; exit; }
 }
-
 // Search in common
 $desc = findInTagsFile('../'.$dir.'tags_'.$lang.'.csv', $fv, $ci);
 if ( !empty($desc) ) { echo $sep.$desc; exit; }
-
-// search cross-sections [cs]
-if ( isset($_GET['cs']) ) {
+// Search cross-sections
+if ( $xs ) {
   for ($i=0;$i<20;$i++) {
     if ( $i===$s ) continue;
     $desc = findInTagsFile('../'.$dir.'tags_'.$lang.'_'.$i.'.csv', $fv, $ci);
     if ( !empty($desc) ) { echo $sep.$desc; exit; }
   }
 }
-
 // No result
 if ( $na ) {
   $L = []; include '../language/'.$lang.'/app_error.php';
